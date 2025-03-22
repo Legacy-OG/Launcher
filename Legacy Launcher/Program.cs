0a6ae64dab0b49630e969492f3e8ddf8a64e7dde
PongoDev <90353497+endlessalpacaYT@users.noreply.github.com>
2025-03-22 17:47:29 +0000

    lots of great new things

--- Legacy Launcher/Program.cs
+++ Legacy Launcher/Program.cs
@@ -10,12 +10,14 @@ using Legacy_Launcher.Utils;
 using System.Net.Http;
 using System.Net;
 using Newtonsoft.Json.Linq;
+using System.IO;
 
 namespace Legacy_Launcher
 {
     internal class Program
     {
         private static string ExchangeCode = "";
+        private static bool PathValid = false;
 
         static void Intro()
         {
@@ -23,21 +25,105 @@ namespace Legacy_Launcher
             Console.WriteLine("Welcome to Legacy launcher!");
             Console.ResetColor();
             Console.WriteLine();
-            Utils.Logger.good("You will be redirected to the Epic Games website to login. After logging in please get the \"authorizationCode\".");
-            Thread.Sleep(2000);
-            Process.Start(new ProcessStartInfo("https://www.epicgames.com/id/api/redirect?clientId=ec684b8c687f479fadea3cb2ad83f5c6&responseType=code") { UseShellExecute = true });
+            if (!string.IsNullOrEmpty(Properties.Settings.Default.access_token) && !string.IsNullOrEmpty(Properties.Settings.Default.refresh_token))
+            {} else
+            {
+                Utils.Logger.good("You will be redirected to the Epic Games website to login. After logging in please get the \"authorizationCode\".");
+                Thread.Sleep(2000);
+                Process.Start(new ProcessStartInfo("https://www.epicgames.com/id/api/redirect?clientId=ec684b8c687f479fadea3cb2ad83f5c6&responseType=code") { UseShellExecute = true });
+            }
         }
 
         static async Task Main(string[] args)
         {
             Intro();
-            Console.Write("Please enter your authorization code: "); // We should check if this is valid
-            string AutorizationCode = Console.ReadLine();
-            await GetAccessToken(AutorizationCode);
-            Console.Write("Please enter your game path: "); // we should also check if this is valid
-            string GamePath = Console.ReadLine();
-            Console.WriteLine();
+            if (!string.IsNullOrEmpty(Properties.Settings.Default.access_token) && !string.IsNullOrEmpty(Properties.Settings.Default.refresh_token))
+            {
+                await RefreshToken(Properties.Settings.Default.refresh_token);
+            } else
+            {
+                Console.Write("Please enter your authorization code: ");
+                string AutorizationCode = Console.ReadLine();
+                await GetAccessToken(AutorizationCode);
+            }
+            string GamePath = await DetermineGamePath();
+            await GetExchangeCode(Properties.Settings.Default.access_token);
             LaunchService.LaunchService.InitializeLaunching(Program.ExchangeCode, GamePath, 0);
+        }
+
+        static async Task<string> DetermineGamePath()
+        {
+            Console.WriteLine("Do you want to update your game path?");
+            Console.Write("[1]: Yes | [2]: No: ");
+            string choice = Console.ReadLine();
+            string GamePath = "";
+            if (choice == "1")
+            {
+                while (!PathValid)
+                {
+                    Console.Write("Please enter your game path: ");
+                    GamePath = Console.ReadLine();
+                    if (File.Exists(Path.Combine(GamePath, "FortniteGame\\Binaries\\Win64\\", "FortniteClient-Win64-Shipping.exe")))
+                    {
+                        PathValid = true;
+                    }
+                    else
+                    {
+                        Utils.Logger.warn("Path Invalid, Make sure your path has the \"Engine\" and \"FortniteGame\" Folders inside!");
+                    }
+                    Console.WriteLine();
+                }
+                Properties.Settings.Default.GamePath = GamePath;
+            }
+            else if (choice == "2") 
+            {
+                if (string.IsNullOrEmpty(Properties.Settings.Default.GamePath)) 
+                {
+                    Utils.Logger.warn("No existing gamepath!");
+                    while (!PathValid)
+                    {
+                        Console.Write("Please enter your game path: ");
+                        GamePath = Console.ReadLine();
+                        if (File.Exists(Path.Combine(GamePath, "FortniteGame\\Binaries\\Win64\\", "FortniteClient-Win64-Shipping.exe")))
+                        {
+                            PathValid = true;
+                        }
+                        else
+                        {
+                            Utils.Logger.warn("Path Invalid, Make sure your path has the \"Engine\" and \"FortniteGame\" Folders inside!");
+                        }
+                        Console.WriteLine();
+                    }
+                    Properties.Settings.Default.GamePath = GamePath;
+                }
+            }
+            else
+            {
+                Utils.Logger.warn("Not a correct choice!");
+                if (string.IsNullOrEmpty(Properties.Settings.Default.GamePath))
+                {
+                    Utils.Logger.warn("No existing gamepath!");
+                    while (!PathValid)
+                    {
+                        Console.Write("Please enter your game path: ");
+                        GamePath = Console.ReadLine();
+                        if (File.Exists(Path.Combine(GamePath, "FortniteGame\\Binaries\\Win64\\", "FortniteClient-Win64-Shipping.exe")))
+                        {
+                            PathValid = true;
+                        }
+                        else
+                        {
+                            Utils.Logger.warn("Path Invalid, Make sure your path has the \"Engine\" and \"FortniteGame\" Folders inside!");
+                        }
+                        Console.WriteLine();
+                    }
+                    Properties.Settings.Default.GamePath = GamePath;
+                }
+            }
+
+            Properties.Settings.Default.Save();
+            Utils.Logger.good($"Saved Path: {Properties.Settings.Default.GamePath}");
+            return Properties.Settings.Default.GamePath;
         }
 
         static async Task GetAccessToken(string AuthorizationCode)
@@ -60,7 +146,16 @@ namespace Legacy_Launcher
                 JObject json = JObject.Parse(responseString);
                 if (json.ContainsKey("access_token"))
                 {
-                    await GetExchangeCode(json["access_token"].ToString());
+                    Properties.Settings.Default.access_token = json["access_token"].ToString();
+                    if (json.ContainsKey("refresh_token"))
+                    {
+                        Properties.Settings.Default.refresh_token = json["refresh_token"].ToString();
+                    }
+                    else
+                    {
+                        Utils.Logger.error("Could not get refresh token, Launching will still proceed but automatic login might not work!");
+                    }
+                    Properties.Settings.Default.Save();
                 } else
                 {
                     Utils.Logger.error("Could not get access token!");
@@ -92,6 +187,55 @@ namespace Legacy_Launcher
                     Console.ReadKey();
                     Environment.Exit(1);
                 }
+            }
+        }
+
+        static async Task RefreshToken(string refresh_token)
+        {
+            using (HttpClient client = new HttpClient())
+            {
+                string url = "https://account-public-service-prod.ol.epicgames.com/account/api/oauth/token";
+
+                var requestData = new FormUrlEncodedContent(new[]
+                {
+                    new KeyValuePair<string, string>("grant_type", "refresh_token"),
+                    new KeyValuePair<string, string>("refresh_token", refresh_token),
+                    new KeyValuePair<string, string>("token_type", "eg1"),
+                });
+
+                client.DefaultRequestHeaders.Add("Authorization", "Basic ZWM2ODRiOGM2ODdmNDc5ZmFkZWEzY2IyYWQ4M2Y1YzY6ZTFmMzFjMjExZjI4NDEzMTg2MjYyZDM3YTEzZmM4NGQ=");
+
+                HttpResponseMessage response = await client.PostAsync(url, requestData);
+                string responseString = await response.Content.ReadAsStringAsync();
+                JObject json = JObject.Parse(responseString);
+                if (json.ContainsKey("access_token"))
+                {
+                    Properties.Settings.Default.access_token = json["access_token"].ToString();
+                    if (json.ContainsKey("refresh_token"))
+                    {
+                        Properties.Settings.Default.refresh_token = json["refresh_token"].ToString();
+                    }
+                    else
+                    {
+                        Utils.Logger.error("Could not get refresh token, Launching will still proceed but automatic login might not work!");
+                    }
+                    Properties.Settings.Default.Save();
+                }
+                else
+                {
+                    Utils.Logger.warn("Could not get access token!");
+                    Utils.Logger.good("You will be redirected to the Epic Games website to login. After logging in please get the \"authorizationCode\".");
+                    Thread.Sleep(2000);
+                    Process.Start(new ProcessStartInfo("https://www.epicgames.com/id/api/redirect?clientId=ec684b8c687f479fadea3cb2ad83f5c6&responseType=code") { UseShellExecute = true });
+                    Console.Write("Please enter your authorization code: ");
+                    string AutorizationCode = Console.ReadLine();
+                    await GetAccessToken(AutorizationCode);
+                    string GamePath = await DetermineGamePath();
+                    await GetExchangeCode(Properties.Settings.Default.access_token);
+                    LaunchService.LaunchService.InitializeLaunching(Program.ExchangeCode, GamePath, 0);
+                }
+
+                Utils.Logger.good($"Successfully logged into {json["displayName"].ToString()}");
             }
         }
     }
